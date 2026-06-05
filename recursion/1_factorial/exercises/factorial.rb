f4b314fb45f81708cdb9b97a42c4f587c43504f4
Joshua Bentley <232677218+jbentleyd-png@users.noreply.github.com>
2026-06-05 11:03:57 -0600

    Solved factorial
    I want to do it in less lines without the total parameter.

--- recursion/1_factorial/exercises/factorial.rb
+++ recursion/1_factorial/exercises/factorial.rb
@@ -1,3 +1,14 @@
-def factorial(num)
+# original solve:
+def factorial(num, total = num)
   # write a recursive method for calculating the [factorial](https://simple.wikipedia.org/wiki/Factorial) of a number
+  puts "num = #{num}"
+  return nil if num < 0
+  return 1 if num == 0
+  return total if num == 1
+
+  total = total * (num - 1) 
+  puts "total = #{total}"
+  factorial(num - 1, total)
 end
+
+p factorial(4)
