2493e4c9c1741283a8815046aba06d37c4cacbd1
Joshua Bentley <232677218+jbentleyd-png@users.noreply.github.com>
2026-06-05 15:34:32 -0600

    Sovled total_integers_exercises

--- recursion/3_total_integers/exercises/total_integers_exercises.rb
+++ recursion/3_total_integers/exercises/total_integers_exercises.rb
@@ -1,4 +1,4 @@
-def total_integers(array)
+def total_integers(array, count = 0)
   # Count the total number of integers inside of the given array
   # The array may be nested, and the integers inside these "inner" layers must also be counted
   #
@@ -7,4 +7,9 @@ def total_integers(array)
   # NOTE: you may notice that `Array#flatten` would make quick work of this,
   # but you should implement this method without using it. The tests will check
   # to make sure `#flatten` isn't used.
+  array.each do |e|
+    count += 1 if e.is_a?(Integer)
+    count = total_integers(e, count) if e.is_a?(Array)
+  end
+  count
 end
