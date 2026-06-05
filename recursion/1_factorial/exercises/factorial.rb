6ac211cbe04460900e5b87f31ea06d7225cbafa9
Joshua Bentley <232677218+jbentleyd-png@users.noreply.github.com>
2026-06-05 11:08:45 -0600

    resolved factorial

--- recursion/1_factorial/exercises/factorial.rb
+++ recursion/1_factorial/exercises/factorial.rb
@@ -1,14 +1,21 @@
 # original solve:
-def factorial(num, total = num)
+#def factorial(num, total = num)
   # write a recursive method for calculating the [factorial](https://simple.wikipedia.org/wiki/Factorial) of a number
-  puts "num = #{num}"
+ # puts "num = #{num}"
+ # return nil if num < 0
+ # return 1 if num == 0
+ # return total if num == 1
+
+ # total = total * (num - 1) 
+  #puts "total = #{total}"
+ # factorial(num - 1, total)
+# end
+
+# `p factorial(4)
+
+# new solve: 
+def factorial(num)
   return nil if num < 0
-  return 1 if num == 0
-  return total if num == 1
-
-  total = total * (num - 1) 
-  puts "total = #{total}"
-  factorial(num - 1, total)
+  return 1 if num == 1 || num == 0
+  num * factorial(num - 1)
 end
-
-p factorial(4)
